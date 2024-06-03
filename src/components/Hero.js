65586c0bed5b61b9a32c70f4e5970a04840636af
Love Kumar <lovekumar.200442@gmail.com>
2024-06-03 17:10:46 +0530

    imp

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ const Hero = () => {
             <div className="content">
                 <h1 className="headline">Every order
                     fulfilled, on time.</h1>
-                <p className="subheadline">Say goodbye to supply chain woes with Crest. Crafted by industry experts, it leverages cutting-edge technology, previously exclusive to corporate giants, now tailored for ambitious startups. In essence, we synchronize supply with demand, empowering your growth journey every step of the way.</p>
+                <p className="subheadline">Say goodbye to supply chain woes with CleverBooks. Crafted by industry experts, it leverages cutting-edge technology, previously exclusive to corporate giants, now tailored for ambitious startups. In essence, we synchronize supply with demand, empowering your growth journey every step of the way.</p>
                 <button className="cta-button">Join the Journey</button>
             </div>
         </div>
